ab0c82afc66cde5939f054bccf3b39af9dcce191
Luka Macan <luka.macan@unibo.it>
2025-09-06 17:28:59 +0200

    Add stable silu

--- TargetLibraries/Generic/src/Silu.c
+++ TargetLibraries/Generic/src/Silu.c
@@ -1,8 +1,21 @@
 #include "DeeployBasicMath.h"
 #include <math.h>
 
+static inline float silu_naive(float x) {
+  return x / (1.0f + expf(-x));
+}
+
+static inline float silu_stable(float x) {
+  if (x >= 0.0f) {
+    return x / (1.0f + expf(-x));
+  } else {
+    const float z = expf(x);
+    return x * z / (1.0f + z);
+  }
+}
+
 void Silu(float *data_in, float *data_out, uint32_t size) {
   for (uint32_t i = 0; i < size; i++) {
-    data_out[i] = data_in[i] / (1 + expf(-data_in[i]));
+    data_out[i] = silu_stable(data_in[i]);
   }
 }
